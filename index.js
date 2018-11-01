e068be1a9bff13f95ff28862c3910fd9fe42ca53
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-01 01:05:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-  } else if (string.toUpperCase === strin) {
+   if (string.toUpperCase === string) {
     return "YES INDEED"
-  } else if (string === "I love you, Grandma") {
+    } else if (string === "I love you, Grandma") {
     return "I love you, too"
+  } else {
+    return "I can't hear you"
   }
 }
